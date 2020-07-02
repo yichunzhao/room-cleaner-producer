1c8737b4be956c07cc92584c7886f2c3fed705eb
yichunzhao <zhaoyichun@hotmail.com>
2020-07-02 02:22:31 +0200

    init commit

---
+++ src/main/java/com/ynz/roombooking/producer/RoomClearingApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.roombooking.producer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomClearingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomClearingApplication.class, args);
+    }
+}
